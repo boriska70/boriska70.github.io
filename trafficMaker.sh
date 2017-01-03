216117fba83feb39ad0d3d2b70841218b473ecd7
boriska70@gmail.com <boriska70@gmail.com>
2017-01-03 16:35:50 +0200

    traffic maker

---
+++ trafficMaker.sh
@@ -0,0 +1,7 @@
+while [ true ]; do
+  echo $(date -Iseconds)
+  fakeip=12.14.16.$(( RANDOM % 255 ))
+  echo $fakeip
+  curl --head --header "X-Forwarded-For: $fakeip" http://boriska70.github.io 2>/dev/null | grep HTTP
+  sleep $(( RANDOM % 60 ))
+done
