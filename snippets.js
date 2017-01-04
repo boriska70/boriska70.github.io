81755fbb361baf17c003463d815a8bf043316e3d
boriska70@gmail.com <boriska70@gmail.com>
2017-01-04 10:19:58 +0200

    use debug version of ga

--- snippets.js
+++ snippets.js
@@ -1,4 +1,14 @@
+>>> Add a callback
 var a1 = function() {console.log('This is a1')}
 var a2 = a1
 a1 = function(callback) { a2(); if(callback) {callback()} else console.log('nothing to do') }
 var cb = function() {console.log('From callback!')}
+
+>>> Plugins in GA
+<script>
+ga('create', 'UA-XXXXX-Y', 'auto');
+ga('require', 'linkTracker',{prop1: 'value1'});
+ga('send', 'pageview');
+</script>
+<!--Note: plugin scripts must be included after the tracking snippet. -->
+<script async src="/path/to/link-tracker-plugin.js"></script>
